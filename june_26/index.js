3b8f900302df502ae74cf650b3fb86cd488127ef
cheesycat69 <136364421+cheesycat69@users.noreply.github.com>
2023-06-26 00:50:08 +0530

    Add files via upload

---
+++ june_26/index.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+
+    var blank = document.getElementById("blank");
+    blank.addEventListener("keydown", function (e) {
+        if (e.keyCode===13) {  //checks whether the pressed key is "Enter"
+            validate(e);
+        }
+    });
+    function runConfetti() {
+        var end = Date.now() + (120 * 1000);
+        var colors = ['#bb0000', '#ffffff'];
+
+        (function frame() {
+            confetti({
+                particleCount: 2,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 },
+                colors: colors
+            });
+            confetti({
+                particleCount: 2,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 },
+                colors: colors
+            });
+
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+            }
+        }());
+    }
+
+    function validate(e) {
+        var text = e.target.value;
+        if(text.toLowerCase().trim() === "i want it that way") {
+            runConfetti();
+
+            document.getElementById('blank').readOnly = true;
+            document.getElementById("blank").style.border="1px solid green";
+            document.getElementById("line").style.display="none";
+            document.getElementById("lyrics").style.display="block";
+            document.getElementById("img_here").src="/Volumes/workplace/test/june_26/thatway.jpeg";
+            document.getElementById("video").src="https://www.youtube.com/embed/hLQl3WQQoQ0?autoplay=1";
+            document.getElementById("video_container").style.display="block";
+            document.getElementById("video_heading").innerHTML="<h1>Backstreet Boys - I Want It That Way</h1>";
+        } else {
+            document.getElementById("blank").style.border="1px solid red";
+        }
+        //validation of the input...
+    }
+}
