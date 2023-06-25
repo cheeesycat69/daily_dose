22e45e1c0d0d50681230ca91fa2669c94386e18e
cheesycat69 <136364421+cheesycat69@users.noreply.github.com>
2023-06-25 08:49:25 +0530

    Update index.js

--- june_25/index.js
+++ june_25/index.js
@@ -41,7 +41,7 @@ window.onload = function() {
             document.getElementById("blank").style.border="1px solid green";
             document.getElementById("line").style.display="none";
             document.getElementById("lyrics").style.display="block";
-            document.getElementById("img_here").src="/Volumes/workplace/test/june_25/days.jpeg";
+            document.getElementById("img_here").src="https://raw.githubusercontent.com/cheesycat69/daily_dose/main/june_25/days.jpeg";
             document.getElementById("video").src="https://www.youtube.com/embed/hLQl3WQQoQ0?autoplay=1";
             document.getElementById("video_container").style.display="block";
             document.getElementById("video_heading").innerHTML="<h1>Adele - Someone Like You</h1>";
